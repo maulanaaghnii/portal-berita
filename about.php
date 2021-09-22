8343a40dd175638f45f39cd9b35ebb0a6b6866a5
maulanaaghnii <maulanaaghni.contact@gmail.com>
2021-09-22 08:47:13 +0700

    init commit

---
+++ about.php
@@ -0,0 +1,36 @@
+<?php include 'header.php'; ?>
+<div class="container-fluid">
+	<div class="row">
+		<div class="container konten-wrapper">
+			<div class="panel panel-default">
+				<div class="panel-body main">
+					<div class="col-md-8">
+						<div class="about">
+							<div class="row post-title">
+								<div class="col-sm-12">
+									<h2>TENTANG KAMPUNG SHOLAWAT</h2>
+								</div>
+							</div>
+							<div class="row post-content">
+								<div class="col-sm-12">
+								<h3><b>السَّلاَمُ عَلَيْكُمْ وَرَحْمَةُ اللهِ وَبَرَكَاتُهُ</b></h3>
+            <p>Apa itu Kampung Sholawat? kampung sholawat 
+                adalah sebuah wadah untuk kembali ke illahi 
+                . Merupakan Media dakwah & Informasi tentang islam yang Rahmatan lil 'alamin serta menjadi sarana berbagi informasi antar sesama muslim.
+                Web Kampung Sholawat ini di harapkan menjadi oase di dunia internet yang kita ketahui bersama jangkauannya melewati batas geografis suatu daerah.
+                Kampung Sholawat Naqsyabandi Nazhimiyah Center
+                Selain itu Kampung Sholawat merupakan nama dari sebuah
+                 Zawiyah di Caruban kab.madiun jawa timur yang mana merupakan
+                  tempat tinggal dari Guru Kami Syaikh Mustafa Mas'ud 
+                ,di mana rutin setiap bulan di adakan majlis SULUK SABTU PAHING.</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<?php include 'sidebar.php'; ?>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<?php include 'footer.php';
